11f73b9e5cea580f23c301bc4ba6ed7a353fed05
windweller <leo.niecn@gmail.com>
2023-04-27 08:53:34 -0700

    fix conflict

--- experiments/ensemble_ope/train_c_tasks.sh
+++ experiments/ensemble_ope/train_c_tasks.sh
@@ -1,3 +1,1 @@
-python control_tasks_train.py --dataset cartpole-replay --epochs 25
-python control_tasks_train.py --dataset pendulum-replay --epochs 25
-python control_tasks_train.py --dataset pendulum-random --epochs 25
+python control_tasks_eval.py --dataset cartpole-random --model_dir model_logs/cartpole_policies
